247e82780af73e29b0f589b4c0c9df946e83ee8c
cristake <cristianiosif@me.com>
2015-11-03 00:21:11 +0200

    Updated NewsletterInterface

--- src/Interfaces/NewsletterInterface.php
+++ src/Interfaces/NewsletterInterface.php
@@ -6,6 +6,15 @@
 interface NewsletterInterface
 {
     /**
+     * Delete a newsletter campaign.
+     *
+     * @param $cid
+     *
+     * @return mixed
+     */
+    public function deleteCampaign($cid);
+
+    /**
      * Create a new newsletter campaign.
      *
      * @param $list
@@ -17,6 +26,39 @@ interface NewsletterInterface
     public function createCampaign($list, $subject, $content);
 
     /**
+     * Create a new newsletter campaign.
+     *
+     * @param $cid string
+     * @param $name string
+     * @param $value array
+     *
+     * @return mixed
+     */
+    public function updateCampaign($cid, $name, $value = []);
+
+    /**
+     * Send a test newsletter campaign.
+     *
+     * @param $subject
+     * @param $content
+     * @param $list
+     *
+     * @return mixed
+     */
+    public function sendTestCampaign($cid, $emails = [], $send_type = '');
+
+    /**
+     * Send a newsletter campaign.
+     *
+     * @param $subject
+     * @param $content
+     * @param $list
+     *
+     * @return mixed
+     */
+    public function sendCampaign($cid);
+
+    /**
      * Subscribe the email address to given list.
      *
      * @param $email
@@ -25,6 +67,16 @@ interface NewsletterInterface
      * @return mixed
      */
     public function subscribe($email, $mergeVars = [], $list = '');
+
+    /**
+     * Update a member subscibed to a list
+     *
+     * @param $email
+     * @param array $mergeVars
+     * @param string $list
+     * @return mixed
+     */
+    public function updateMember($email, $mergeVars = [],  $list = '');
 
     /**
      * Unsubscribe the email address to given list.
