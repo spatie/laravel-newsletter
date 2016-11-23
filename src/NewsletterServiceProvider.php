f76e858601759853fc37ea5bbad8725becdf39c4
David Jimenez <d-jimenez@users.noreply.github.com>
2016-11-23 15:22:02 +0100

    NewsletterServiceProvider verify ssl
    Use MAILCHIMP_SSL configuration

--- src/NewsletterServiceProvider.php
+++ src/NewsletterServiceProvider.php
@@ -22,6 +22,8 @@ class NewsletterServiceProvider extends ServiceProvider
     {
         $this->app->singleton(Newsletter::class, function () {
             $mailChimp = new Mailchimp(config('laravel-newsletter.apiKey'));
+            
+            $mailChimp->verify_ssl = config('laravel-newsletter.ssl');
 
             $configuredLists = NewsletterListCollection::createFromConfig(config('laravel-newsletter'));
 
