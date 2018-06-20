d8251e5f9f374ab52d7b888102cea716181d7e31
Andriy Klitsuk <andriy@idra.intranet.risolviamo.net>
2018-06-20 18:21:03 +0200

    add send campaign

---
+++ src/Exceptions/CampaignException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Spatie\Newsletter\Exceptions;
+
+use Exception;
+
+class CampaignException extends Exception
+{
+    /**
+     * @return static
+     */
+    public static function withMessage($message)
+    {
+        return new static($message);
+    }
+}
--- src/Newsletter.php
+++ src/Newsletter.php
@@ -3,6 +3,7 @@
 namespace Spatie\Newsletter;
 
 use DrewM\MailChimp\MailChimp;
+use Spatie\Newsletter\Exceptions\CampaignException;
 
 class Newsletter
 {
@@ -191,6 +192,23 @@ class Newsletter
         $options = array_merge($defaultOptions, $options);
 
         $response = $this->mailChimp->put("campaigns/{$campaignId}/content", $options);
+
+        if (! $this->lastActionSucceeded()) {
+            return false;
+        }
+
+        return $response;
+    }
+
+    public function sendCampaign(string $campaignId)
+    {
+        $response = $this->mailChimp->post("campaigns/{$campaignId}/actions/send");
+
+        $lastResponse = json_decode(($this->mailChimp->getLastResponse()['body']));
+
+        if ($lastResponse->status !== 200) {
+            throw CampaignException::withMessage($lastResponse->detail);
+        }
 
         if (! $this->lastActionSucceeded()) {
             return false;
