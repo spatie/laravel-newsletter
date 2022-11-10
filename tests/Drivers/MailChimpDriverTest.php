b5966ac898e301c596fbe228ae9fa10279715179
freekmurze <freekmurze@users.noreply.github.com>
2022-11-10 14:07:39 +0000

    Fix styling

--- tests/Drivers/MailChimpDriverTest.php
+++ tests/Drivers/MailChimpDriverTest.php
@@ -4,7 +4,7 @@ use DrewM\MailChimp\MailChimp;
 use Spatie\Newsletter\Drivers\MailChimpDriver;
 use Spatie\Newsletter\Facades\Newsletter;
 
-it('can get the MailChimp API', function() {
+it('can get the MailChimp API', function () {
     config()->set('newsletter.driver', MailChimpDriver::class);
 
     expect(Newsletter::getApi())->toBeInstanceOf(MailChimp::class);
