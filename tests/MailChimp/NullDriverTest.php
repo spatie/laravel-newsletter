b77faced260c51f20be77a4b3fcfb6f3c20ef328
Alex Manase <malexandru699@gmail.com>
2022-11-10 18:17:14 +0200

    refactor `NullDriverTest`

--- tests/MailChimp/NullDriverTest.php
+++ tests/MailChimp/NullDriverTest.php
@@ -1,42 +1,32 @@
 <?php
 
-namespace Spatie\Newsletter\Test;
-
 use Illuminate\Support\Facades\Log;
 use Spatie\Newsletter\NullDriver;
 
-class NullDriverTest extends \PHPUnit\Framework\TestCase
-{
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-        \Mockery::close();
-    }
+afterEach(function () {
+    Mockery::close();
+});
 
-    /** @test */
-    public function it_can_be_call_with_any_method()
-    {
-        $subject = new NullDriver();
+it('can be call with any method', function () {
+    $subject = new NullDriver();
 
-        $this->assertNull($subject->whatever());
-        $this->assertNull($subject->subscription());
-        $this->assertNull($subject->addTags('jason@testing.com', ['tags']));
-    }
+    expect($subject)
+        ->whatever()->toBeNull()
+        ->subscription()->toBeNull()
+        ->addTags('jason@testing.com', ['tags'])->toBeNull();
+});
 
-    /** @test */
-    public function it_logs_the_method_call_when_log_is_set()
-    {
-        $subject = new NullDriver(true);
+it('logs the method call when log is set', function () {
+    $subject = new NullDriver(true);
 
-        $log = \Mockery::mock();
-        Log::swap($log);
+    $log = \Mockery::mock();
+    Log::swap($log);
 
-        $log->shouldReceive('debug')->twice();
+    $log->shouldReceive('debug')->twice();
 
-        $this->assertNull($subject->whatever());
-        $this->assertNull($subject->addTags('jason@testing.com', ['tags']));
+    expect($subject->whatever())
+        ->and($subject->addTags('jason@testing.com', ['tags']))->toBeNull();
 
-        $log->shouldHaveReceived('debug', ['Called Spatie Newsletter facade method: whatever with:', []]);
-        $log->shouldHaveReceived('debug', ['Called Spatie Newsletter facade method: addTags with:', ['jason@testing.com', ['tags']]]);
-    }
-}
+    $log->shouldHaveReceived('debug', ['Called Spatie Newsletter facade method: whatever with:', []]);
+    $log->shouldHaveReceived('debug', ['Called Spatie Newsletter facade method: addTags with:', ['jason@testing.com', ['tags']]]);
+});
