229af69402794ce8e6ba3b5002cfbedd91d4a24b
freekmurze <freekmurze@users.noreply.github.com>
2022-11-10 13:31:57 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Spatie\Newsletter\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
-use Spatie\MailcoachSdk\MailcoachSdkServiceProvider;
 use Spatie\Newsletter\NewsletterServiceProvider;
 
 class TestCase extends Orchestra
