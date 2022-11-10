22930fc204435ed50014ded3e4bf5d2045ca5b5c
Freek Van der Herten <freek@spatie.be>
2022-11-10 23:42:10 +0100

    wip

--- tests/Drivers/MailChimpDriverTest.php
+++ tests/Drivers/MailChimpDriverTest.php
@@ -7,5 +7,9 @@ use Spatie\Newsletter\Facades\Newsletter;
 it('can get the MailChimp API', function () {
     config()->set('newsletter.driver', MailChimpDriver::class);
 
+    // this avoids MailChimp from throwing an exception
+    // without an API key
+    config()->set('newsletter.driver_arguments.endpoint', '');
+
     expect(Newsletter::getApi())->toBeInstanceOf(MailChimp::class);
 });
