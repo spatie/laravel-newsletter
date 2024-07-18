0507ee59ab0887d5af7c427081ecfdc9a815bd29
Keller Martin <43446570+kellerjmrtn@users.noreply.github.com>
2024-07-18 11:01:44 -0400

    Reintroduce NullDriver From v4 (#323)
    * Reintroduce NullDriver From v4
    
    * Update composer.json
    
    ---------
    
    Co-authored-by: Keller Martin <kellerm@schwebdesign.com>
    Co-authored-by: Freek Van der Herten <freek@spatie.be>

--- src/NewsletterServiceProvider.php
+++ src/NewsletterServiceProvider.php
@@ -5,6 +5,7 @@ namespace Spatie\Newsletter;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Spatie\Newsletter\Drivers\Driver;
+use Spatie\Newsletter\Drivers\NullDriver;
 use Spatie\Newsletter\Support\Lists;
 
 class NewsletterServiceProvider extends PackageServiceProvider
@@ -21,6 +22,14 @@ class NewsletterServiceProvider extends PackageServiceProvider
         $this->app->singleton('newsletter', function () {
             /** @var class-string<Driver> $driverClass */
             $driverClass = config('newsletter.driver');
+
+            if (
+                is_null($driverClass)
+                || $driverClass === 'log'
+                || $driverClass === NullDriver::class
+            ) {
+                return new NullDriver($driverClass === 'log');
+            }
 
             $arguments = config('newsletter.driver_arguments');
             $lists = Lists::createFromConfig(config('newsletter'));
